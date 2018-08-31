21674d1211b6d50e145bebcbddc79cddb21280df
Макс Недашковский <max0350@gmail.com>
2018-08-31 04:25:30 +0300

    added animation to routes

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -2,7 +2,15 @@
   <v-app v-if="$auth.ready() && loaded">
     <template v-if="$auth.check()">
         <appNavigation>
-            <router-view :key="$route.fullPath"></router-view>
+            <transition
+                appear
+                enter-class=""
+                enter-active-class="animated fadeInDown"
+                leave-class=""
+                leave-active-class="animated fadeOutDown"
+                mode="out-in">
+                <router-view :key="$route.fullPath"></router-view>
+            </transition>
         </appNavigation>
     </template>
     <template v-else>
@@ -42,13 +50,55 @@ export default {
 
 
 <style>
-
     .bg-primary {
         width: 100%;
         height: 100vh;
         background: rgb(8,54,94);
         background: linear-gradient(40deg, rgba(8,54,94,1) 0%, rgba(32,100,151,1) 100%);
         background-size: cover;
+    }
+
+    .animated {
+        animation-duration: 0.3s;
+        animation-fill-mode: both;
+    }
+
+    @keyframes fadeInDown {
+        from {
+            opacity: 0;
+            transform: translate3d(0, -10%, 0);
+        }
+
+        to {
+            opacity: 1;
+            transform: translate3d(0, 0%, 0);
+        }
+    }
+
+    .fadeInDown {
+        animation-name: fadeInDown;
+    }
+
+    @keyframes fadeOutDown {
+        from {
+            opacity: 1;
+            transform: translate3d(0, 0%, 0);
+        }
+        to {
+            opacity: 0;
+            transform: translate3d(0, 10%, 0);
+        }
+    }
+
+    .fadeOutDown {
+        animation-name: fadeOutDown;
+    }
+
+    @media (prefers-reduced-motion) {
+        .animated {
+            animation: unset !important;
+            transition: none !important;
+        }
     }
 
 </style>
